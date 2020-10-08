ba980494bc26585a0d40439dfb53213202343e75
Phenomite <8285537+Phenomite@users.noreply.github.com>
2020-10-08 19:54:50 -0400

    add fft stego script

---
+++ scripts/fftstego_picture.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+# Run this script against an image file to reveal potential FFT based stego
+
+if len(sys.argv)==1:
+    print("needs file path as first arg")
+
+path = sys.argv[1]
+
+cmaptypes_available = ['Accent', 'Accent_r', 'Blues', 'Blues_r', 'BrBG', 'BrBG_r', 'BuGn', 'BuGn_r', 'BuPu', 'BuPu_r', 'CMRmap', 'CMRmap_r', 'Dark2', 'Dark2_r', 'GnBu', 'GnBu_r', 'Greens', 'Greens_r', 'Greys', 'Greys_r', 'OrRd', 'OrRd_r', 'Oranges', 'Oranges_r', 'PRGn', 'PRGn_r', 'Paired', 'Paired_r', 'Pastel1', 'Pastel1_r', 'Pastel2', 'Pastel2_r', 'PiYG', 'PiYG_r', 'PuBu', 'PuBuGn', 'PuBuGn_r', 'PuBu_r', 'PuOr', 'PuOr_r', 'PuRd', 'PuRd_r', 'Purples', 'Purples_r', 'RdBu', 'RdBu_r', 'RdGy', 'RdGy_r', 'RdPu', 'RdPu_r', 'RdYlBu', 'RdYlBu_r', 'RdYlGn', 'RdYlGn_r', 'Reds', 'Reds_r', 'Set1', 'Set1_r', 'Set2', 'Set2_r', 'Set3', 'Set3_r', 'Spectral', 'Spectral_r', 'Wistia', 'Wistia_r', 'YlGn', 'YlGnBu', 'YlGnBu_r', 'YlGn_r', 'YlOrBr', 'YlOrBr_r', 'YlOrRd', 'YlOrRd_r', 'afmhot', 'afmhot_r', 'autumn', 'autumn_r', 'binary', 'binary_r', 'bone', 'bone_r', 'brg', 'brg_r', 'bwr', 'bwr_r', 'cividis', 'cividis_r', 'cool', 'cool_r', 'coolwarm', 'coolwarm_r', 'copper', 'copper_r', 'cubehelix', 'cubehelix_r', 'flag', 'flag_r', 'gist_earth', 'gist_earth_r', 'gist_gray', 'gist_gray_r', 'gist_heat', 'gist_heat_r', 'gist_ncar', 'gist_ncar_r', 'gist_rainbow', 'gist_rainbow_r', 'gist_stern', 'gist_stern_r', 'gist_yarg', 'gist_yarg_r', 'gnuplot', 'gnuplot2', 'gnuplot2_r', 'gnuplot_r', 'gray', 'gray_r', 'hot', 'hot_r', 'hsv', 'hsv_r', 'inferno', 'inferno_r', 'jet', 'jet_r', 'magma', 'magma_r', 'nipy_spectral', 'nipy_spectral_r', 'ocean', 'ocean_r', 'pink', 'pink_r', 'plasma', 'plasma_r', 'prism', 'prism_r', 'rainbow', 'rainbow_r', 'seismic', 'seismic_r', 'spring', 'spring_r', 'summer', 'summer_r', 'tab10', 'tab10_r', 'tab20', 'tab20_r', 'tab20b', 'tab20b_r', 'tab20c', 'tab20c_r', 'terrain', 'terrain_r', 'turbo', 'turbo_r', 'twilight', 'twilight_r', 'twilight_shifted', 'twilight_shifted_r', 'viridis', 'viridis_r', 'winter', 'winter_r']
+
+def two_d_FFT(cmapMode):
+    # Load the image in grayscale mode
+    img = cv2.imread(path, cv2.IMREAD_GRAYSCALE) #
+    # Compute the two-dimensional FFT.
+    f = np.fft.fft2(img)
+    # Swap half-spaces for all axes
+    fshift = np.fft.fftshift(f)
+    # Calculate the logarithm
+    magnitude_spectrum = 20*np.log(np.abs(fshift))
+    plt.title(cmapMode)
+    plt.imshow(magnitude_spectrum, cmap=cmapMode)
+
+plt.figure()
+plt.subplot(221)
+two_d_FFT('Accent')
+plt.subplot(222)
+two_d_FFT('rainbow')
+plt.subplot(223)
+two_d_FFT('binary')
+plt.subplot(224)
+two_d_FFT('Reds')
+
+# Display the result
+plt.show()
